c26e38569ad4606762da63c02873f84025872a10
MarcioRMed <contato.microinfo@gmail.com>
2022-03-11 08:29:39 -0300

    upgrade

--- NodeEJS/server.js
+++ NodeEJS/server.js
@@ -1,25 +1,28 @@
+
 const express = require('express'); //importa modulo servidor rotas
 const path = require("path"); // importa modulo caminhos arquivos
-
-
-
 const app = express(); // instancia app
 
-app.set('view engine', 'ejs'); //local dos arquivos ejs
+app.set('view engine', 'ejs'); //local dos arquivos ejs dentro  dapasta view
 
 app.use(express.static(path.join(__dirname, "public"))); //configuração funcionar o css, javascript
 
 app.get('/', function (req, res) {  //rota principal
-  res.render('index');
+  res.render('pages/index');
 })
 
 app.get('/sobre', function (req, res) { //sub pasta
-  res.render('about');
+  res.render('pages/about');
 })
 
+
 // localhost:8080
-app.listen(8080);
-console.log('Rodando...')
+app.listen(8080, () => console.log('Rodando...'));
+
+
+
+
+
 
 
 //qualquer alteração nos arquivos js precisar inicar o servidor
